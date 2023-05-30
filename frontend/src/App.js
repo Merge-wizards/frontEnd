9ec5e4dea8b3773c814f57ba80d684b9e472eeb0
NSALAA9 <alaansairat99@gmail.com>
2023-05-30 04:41:30 +0000

    About US page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/footer/Footer';
 import Hero from './components/hero/Hero'
 import Navbar from './components/nav/Navbar'
 import PcCard from './components/pc/PcCard';
+import Team from './components/Team/team';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
     <Hero/>
     <PcCard/>
     <Browser/>
+    <Team/>
     <Footer/>
     </>
   );
